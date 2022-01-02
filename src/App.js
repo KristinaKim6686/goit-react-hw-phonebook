a307d30ef1e74fb97951a92b634914f702069a24
kim <82406962+KristinaKim6686@users.noreply.github.com>
2022-01-02 19:22:38 +0300

    styling

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import { Component } from "react";
+import { nanoid } from "nanoid";
+import { Container, MainHeader, Header } from "./App.styled";
+import ContactForm from "./Components/ContactForm";
+import ContactList from "./Components/ContactList";
+import Filter from "./Components/Filter";
+import FilterContacts from "./utils/contactFilter";
+import filterContacts from "./utils/contactFilter";
+import backgroundImage from "./img/winter-gc4450ba70_1920.jpg";
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ],
+    filter: "",
+  };
+  handleAddContact = (data) => {
+    // const { contacts } = this.state;
+    const { name, number } = data;
+    const inputId = nanoid();
+    const contact = {
+      name,
+      number,
+    };
+    if (this.state.contacts.some((el) => el.name === contact.name)) {
+      alert(this.state.name + " is already in contacts");
+      return;
+    }
+
+    this.setState((prev) => ({
+      contacts: [...prev.contacts, { ...contact, id: inputId }],
+    }));
+  };
+  handleDeleteContact = (event) => {
+    this.setState({
+      contacts: this.state.contacts.filter(
+        (contact) => contact.id !== event.target.id
+      ),
+    });
+  };
+  handleFilterContact = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  render() {
+    const contacts = filterContacts(this.state.contacts, this.state.filter);
+    return (
+      <Container
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+        }}
+      >
+        <MainHeader>Phonebook</MainHeader>
+        <ContactForm handleAddContact={this.handleAddContact}></ContactForm>
+        <Header>Contacts</Header>
+        <Filter
+          filterValue={this.state.filter}
+          onFilter={this.handleFilterContact}
+        />
+        <ContactList
+          contacts={contacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
